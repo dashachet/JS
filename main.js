9ea9e1e26c7c4324f48d5d96b271fceafefb313d
dashachet <drdashashasha@gmail.com>
2024-11-15 08:27:15 +0200

    commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // push() , pop(), shift(), unshift()
 
 
-const names = ['Alex', 'Bob', 'Ivan', 'Sasha']; 
+const names = ['Alex', 'Bob', "Ola", 'Ivan', 'Sasha']; 
 console.log('names', names) ;
 const allnames = names.push('Mary');
 
@@ -40,3 +40,97 @@ const reversetelephone = splittelephone.reverse();
 console.log(reversetelephone);
 const jointelephone = reversetelephone.join('');
 console.log(jointelephone);
+
+
+const arr1 = [1, 2]
+const arr2 = [3, 4]
+const arr3 = [5, 6]
+
+const concatarr = arr1.concat(arr2, arr3, 7, 8); // метод массива concat сливает указанные массивы в один общий массив. Метод применяется к одному из массивов, а в параметрах метода передаются остальные массивы для слияния. При этом метод не изменяет исходный массив, а возвращает новый. Если вы передадите в concat() массив с вложенными массивами, они также будут объединены в новый массив. Вложенные массивы не будут "раскрыты" или "сплющены", они будут сохранены внутри нового массива как элементы. можно передавать обычные значения
+
+console.log(concatarr)
+
+
+
+const nestedArr = [
+    [5, 6],
+    [7, 8],
+]
+
+const result = arr1.concat(arr2, nestedArr)
+const flatresult = result.flat(Infinity);
+
+console.log(result);
+console.log(flatresult);
+
+
+// forEach() - позволяет последовательно перебрать все элементы массива.Метод в параметре получает функцию, которая выполнится для каждого элемента массива.В эту функцию можно передавать три параметра.Если эти параметры есть(они не обязательны), то в первый автоматически попадет элемент массива, во второй попадет его номер в массиве(индекс), а в третий - сам массив.
+
+
+// //массив.forEach(function(элемент, индекс, массив) {
+// код, который выполнится для всех элементов
+// });
+
+
+const array = [1, 2, 3, 4, 5]
+let sum = 0
+
+arr.forEach(function(el) {
+    sum += el// sum= sum + el; 
+
+
+});
+
+// умножение на 2
+
+const numbers = [2, 5, 10];
+
+numbers.forEach(function(el, index, array) {
+    array[index] = el * 2;
+    // array[index]= array[index] * 2;
+    
+})
+
+console.log(numbers);
+
+// indexOf() - осуществляет поиск элемента в массиве.В первом параметре указываем номер позиции искомого символа.Метод возвращает номер первого найденного элемента, либо - 1, если такого элемента нет.Второй необязательный параметр метода задает позицию, с которой следует начинать поиск.
+
+// Давайте найдем позицию первой тройки в массиве:
+
+
+const numbersindex = [1, 2, 3, 3, 3, 4, 5]
+const res = numbersindex.indexOf(3, -2);
+
+console.log(res) // 2
+
+// lastIndexOf() - осуществляет поиск элемента в массиве.Возвращает номер последнего найденного элемента, либо - 1, если такого элемента нет.Первым параметром мы указываем номер элемента, вторым(необязательным) - позицию, с которой следует начинать поиск.Поиск ведется с конца массива в начало.
+
+
+let lastindexnumbers = [1, 2, 3, 3, 4, 5, 3]
+let res1 = lastindexnumbers.lastIndexOf(3, 4)
+
+console.log(res1) // 3
+
+// find() - Метод find помогает найти первый элемент в массиве согласно переданному в параметре коллбэку.Если элемента нет, то возвращается undefined.
+
+// найти элемент массива, длина которого равна 1, 3, 4:
+
+const namesclass = ['Alex', 'Bob', "Ola", 'Ivan', 'Sasha']; 
+
+const findNames = namesclass.find(function(el) {
+    return el.length == 1;
+}) 
+
+console.log(findNames);
+
+const findNames1 = namesclass.find(function(el) {
+        return el.length == 3;
+})
+
+console.log(findNames1);
+
+const findNames2 = namesclass.find(function(el) {
+    return el.length === 4;
+});
+
+console.log(findNames2);
