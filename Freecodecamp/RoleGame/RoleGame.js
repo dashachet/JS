2e780a2412906e98ac7e1aac5fa0809f5c60c853
dashachet <drdashashasha@gmail.com>
2024-11-19 20:44:30 +0200

    J

--- Freecodecamp/RoleGame/RoleGame.js
+++ Freecodecamp/RoleGame/RoleGame.js
@@ -55,11 +55,20 @@ function goStore() {
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {
     console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    }
 }
 
 function buyHealth() {
