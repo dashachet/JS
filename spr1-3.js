348c3cdfe1ed4d1c6e4da8b0f54d44f0f941024c
dashachet <drdashashasha@gmail.com>
2024-11-27 10:58:09 +0200

    MJS

--- spr1-3.js
+++ spr1-3.js
@@ -3,11 +3,47 @@
 
 let arr = ['a', 'b', 'c', 'd', 'e']
 const slicearr = arr.slice(1, 3);
-console.log(slicearr);
+// console.log(slicearr);
 const slice1arr = arr.slice(1, -1);
-console.log(slice1arr);
+// console.log(slice1arr);
+
+function itSlice(startI, endI) {
+    const result = [];
+    const end = endI || this.length;
+    const start = startI || 0;
+    for (let i = start; i < end; i++) {
+        result.push(this[i]);
+    }
+    return result;
+}
+// console.log(itSlice(arr, 1, 3));
+// itSlice(arr, 1, 3);
+
+const num = [1, 2, 3,4, 5, 6, 7, 8];
+const nums = new Array(1, 2, 3, 4, 5, 6, 7, 8); // сущнность класс array
+
+Array.prototype.itSlice = itSlice;
+
+nums.itSlice(2, 6);
+
+console.log(nums.itSlice(1,3));
 
 
+// filter => itFilter
+
+
+Array.prototype.itFilter = function(checkFunction) {
+    const result = [];
+    for (let index = 0; index < this.length; index++) {
+        if (checkFunction(this[index])) {
+            result.push(this[index]);
+        }
+        
+    }
+
+    return result;
+
+}
 // splice() - удаляет или добавляет элементы в массив.Можно только удалять элементы, только добавлять или делать и то и другое одновременно; изменяет сам массив и возвращает при этом массив удаленных элементов.
 
 // Первым параметром метод принимает номер элемента массива, который нужно удалить.Вторым параметром - сколько элементов массива следует удалить.Если его поставить в 0 - то элементы удалены не будут(только добавлены новые).Дальше через запятую идут элементы, которые нужно добавить в массив(являются необязательными параметрами).Эти элементы добавятся вместо удаленных элементов массива.
