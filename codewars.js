c1f4d0b00ae9ec16c735690aca855fb100b6772a
dashachet <drdashashasha@gmail.com>
2024-12-02 09:41:38 +0200

    C

--- codewars.js
+++ codewars.js
@@ -17,3 +17,13 @@ function eachCons(array, n) {
     }
     return r;
 }
+
+
+
+
+// Implement String#digit ? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit(0 - 9), false otherwise.
+
+
+String.prototype.digit = function() {
+  return this.length === 1 && '0' <= this && this <= '9';
+};
