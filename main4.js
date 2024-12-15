e3bfa3cd82c6df849323f48d142443e146110302
dashachet <drdashashasha@gmail.com>
2024-12-15 09:38:23 +0200

    JS4

---
+++ main4.js
@@ -0,0 +1,23 @@
+const user = {
+    name: "Bob",
+    age: 32
+}
+
+function getObject (key,value) {
+    const newObject = {}
+    newObject[key] = value
+    return newObject;
+
+    // return {
+    //     [key] : value
+    // }
+}
+
+console.log(getObject("name", "Bob"));
+
+function createObject(key, value) {
+    return {
+        [key.toUpperCase()]: value,
+    }
+}
+createObject('car', 'Audi')
