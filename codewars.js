a89c7d8a8f92bdaba0d66c708937362f23219ae6
dashachet <drdashashasha@gmail.com>
2024-12-22 09:04:26 +0200

    Cdwrs

--- codewars.js
+++ codewars.js
@@ -49,3 +49,13 @@ function evenNumbers(array, n) {
     const reveseArr = newArr.slice(-n)
     return reveseArr
 }
+
+
+// Given a mathematical equation that has *, +, -,/, reverse it as follows:
+
+// solve("100*b/y") = "y/b*100"
+// solve("a+b-c/d*30") = "30*d/c-b+a"
+
+function solve(eq) {
+    return eq.split(/([*+-/])/).reverse().join('')
+}
