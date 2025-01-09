5fe1ab6e2954ffeae851ac5cd944fc804e272b37
dashachet <drdashashasha@gmail.com>
2025-01-09 19:59:43 +0200

    K

--- codewars.js
+++ codewars.js
@@ -59,3 +59,25 @@ function evenNumbers(array, n) {
 function solve(eq) {
     return eq.split(/([*+-/])/).reverse().join('')
 }
+
+// In this kata, we will make a function to test whether a period is late.
+
+// Our function will take three parameters:
+
+// last - The Date object with the date of the last period
+
+// today - The Date object with the date of the check
+
+// cycleLength - Integer representing the length of the cycle in days
+
+// Return true if the number of days passed from last to today is greater than cycleLength.Otherwise, return false.
+
+
+
+const periodIsLate = (last, today, cycleLength) => {
+    const numberDaysLast = last.getTime() / (1000 * 3600 * 24)
+    const numberDaysToday = today.getTime() / (1000 * 3600 * 24)
+    const diferenceDays = numberDaysToday - numberDaysLast
+
+    return diferenceDays > cycleLength
+}
